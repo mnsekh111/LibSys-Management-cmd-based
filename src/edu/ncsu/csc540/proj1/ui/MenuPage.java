bfe718c24ec94ac430ab90b3804b4cb51ffe2edc
Dixon Crews <dccrews@ncsu.edu>
2015-10-22 13:48:29 -0400

    Updated menus, added student/faculty menus

--- src/edu/ncsu/csc540/proj1/ui/MenuPage.java
+++ src/edu/ncsu/csc540/proj1/ui/MenuPage.java
@@ -4,28 +4,35 @@ import java.util.Scanner;
 
 public class MenuPage {
 
-	public MenuPage(){
-		
-	}
-	
-	public int loginMenu(Scanner in){
-		System.out.println("Please select an option : ");
-		System.out.println("1. Login ");
-		int option = in.nextInt();
-		int user_id = 0;
-		if(option == 1){
-			System.out.println("Please enter your Login ID");
-			user_id = in.nextInt();
-			return user_id;
-		}else{
-			System.out.println("Please select a valid option");
-			return 0;
-		}
-		
-	}
-	
-	public void mainMenu(Scanner in){
-		
-	}
+    public MenuPage() {
+
+    }
+
+    public int loginMenu(Scanner in) {
+        int user_id = 0;
+        System.out.print("Please enter your ID number to login: ");
+        user_id = in.nextInt();
+        return user_id;
+    }
+
+    public void studentMenu(Scanner in) {
+        System.out.println("Please select an option:");
+        System.out.println("\t1. Profile");
+        System.out.println("\t2. Resources");
+        System.out.println("\t3. Checked-Out Resources");
+        System.out.println("\t4. Resource Requests");
+        System.out.println("\t5. Notifications");
+        System.out.println("\t6. Balance Due");
+    }
+
+    public void facultyMenu(Scanner in) {
+        System.out.println("Please select an option:");
+        System.out.println("\t1. Profile");
+        System.out.println("\t2. Resources");
+        System.out.println("\t3. Checked-Out Resources");
+        System.out.println("\t4. Resource Requests");
+        System.out.println("\t5. Notifications");
+        System.out.println("\t6. Balance Due");
+    }
 }
 
