e53f2ccb620392659ac6c98236c6f5e2abc1d1d1
Dixon Crews <dccrews@ncsu.edu>
2015-10-25 16:18:21 -0400

    Student profile now displays all pertinent information, editing still needed

--- src/edu/ncsu/csc540/proj1/ui/MenuPage.java
+++ src/edu/ncsu/csc540/proj1/ui/MenuPage.java
@@ -2,11 +2,11 @@ package edu.ncsu.csc540.proj1.ui;
 
 import java.util.Scanner;
 
+import edu.ncsu.csc540.proj1.models.Student;
+
 public class MenuPage {
 
-    public MenuPage() {
-
-    }
+    Student student = new Student();
 
     public int loginMenu(Scanner in) {
         int user_id = 0;
@@ -16,33 +16,44 @@ public class MenuPage {
     }
 
     public void studentMenu(Scanner in, int patronId) {
-    	int selectedOption = 0;
-    	do{
-    		System.out.println("Please select an option:");
-    		System.out.println("\t1. Profile");
-    		System.out.println("\t2. Resources");
-    		System.out.println("\t3. Checked-Out Resources");
-    		System.out.println("\t4. Resource Requests");
-    		System.out.println("\t5. Notifications");
-    		System.out.println("\t6. Balance Due");
-    		System.out.println("\t7. Logout");
-    		selectedOption = in.nextInt();
-    	}while(selectedOption != 7);
+        int selectedOption = 0;
+        do {
+            System.out.println("Please select an option:");
+            System.out.println("\t1. Profile");
+            System.out.println("\t2. Resources");
+            System.out.println("\t3. Checked-Out Resources");
+            System.out.println("\t4. Resource Requests");
+            System.out.println("\t5. Notifications");
+            System.out.println("\t6. Balance Due");
+            System.out.println("\t7. Logout\n");
+            selectedOption = in.nextInt();
+
+            switch(selectedOption) {
+            case 1:
+                studentProfileMenu(in, patronId);
+            }
+        } while(selectedOption != 7);
+    }
+
+    public void studentProfileMenu(Scanner in, int patronID) {
+        System.out.println("==========\nMy Profile:\n");
+        student.printStudentProfile(patronID);
+        System.out.println("\n==========\n");
     }
 
     public void facultyMenu(Scanner in, int patronId) {
-    	int selectedOption = 0;
-    	do{
-    		System.out.println("Please select an option:");
-    		System.out.println("\t1. Profile");
-    		System.out.println("\t2. Resources");
-    		System.out.println("\t3. Checked-Out Resources");
-    		System.out.println("\t4. Resource Requests");
-    		System.out.println("\t5. Notifications");
-    		System.out.println("\t6. Balance Due");
-    		System.out.println("\t7. Logout");
-    		selectedOption = in.nextInt();
-    	}while(selectedOption != 7);
+        int selectedOption = 0;
+        do {
+            System.out.println("Please select an option:");
+            System.out.println("\t1. Profile");
+            System.out.println("\t2. Resources");
+            System.out.println("\t3. Checked-Out Resources");
+            System.out.println("\t4. Resource Requests");
+            System.out.println("\t5. Notifications");
+            System.out.println("\t6. Balance Due");
+            System.out.println("\t7. Logout\n");
+            selectedOption = in.nextInt();
+        } while(selectedOption != 7);
     }
 }
 
