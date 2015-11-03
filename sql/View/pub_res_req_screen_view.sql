fee4684757ad906e3aa89dc019c6ba81f23cfd0b
bharathimit <bharathi.mit@gmail.com>
2015-11-03 17:16:41 -0500

    Close bracket

--- sql/View/pub_res_req_screen_view.sql
+++ sql/View/pub_res_req_screen_view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW PUB_RES_REQ_SCREEN_VIEW
 AS
-  SELECT pq.cid,pq.PATRONID,cpsv.TITLE,cpsv.COPY_TYPE
+  (SELECT pq.cid,pq.PATRONID,cpsv.TITLE,cpsv.COPY_TYPE
   FROM Copies_pub_screen_view cpsv,Pub_queue pq
-  WHERE cpsv.id = pq.cid;---
+  WHERE cpsv.id = pq.cid);---
