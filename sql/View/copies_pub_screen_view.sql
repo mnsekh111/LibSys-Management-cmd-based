52ac04526e2787566940c8354ed3ae11ce8096ec
Dixon Crews <dccrews@ncsu.edu>
2015-11-03 17:12:17 -0500

    Delimeter changes

--- sql/View/copies_pub_screen_view.sql
+++ sql/View/copies_pub_screen_view.sql
@@ -2,4 +2,4 @@ CREATE OR REPLACE VIEW COPIES_PUB_SCREEN_VIEW
 AS
   SELECT c.ID,c.pid,p.title,c.COPY_TYPE
   FROM Copies c,Publications p
-  WHERE c.pid = p.id;
+  WHERE c.pid = p.id;---
--- sql/View/pub_check_out_screen_view.sql
+++ sql/View/pub_check_out_screen_view.sql
@@ -1,3 +1,3 @@
 create or replace view PUB_CHECK_OUT_SCREEN_VIEW as
   select cpsv.id,cpsv.pid,cpsv.TITLE,cpsv.COPY_TYPE,co.START_TIME,co.END_TIME,co.PATRON_ID from CHECKS_OUT co,COPIES_PUB_SCREEN_VIEW cpsv
-  where co.copy_id = cpsv.Id and co.ACT_RETURN_TIME is null;
+  where co.copy_id = cpsv.Id and co.ACT_RETURN_TIME is null;---
